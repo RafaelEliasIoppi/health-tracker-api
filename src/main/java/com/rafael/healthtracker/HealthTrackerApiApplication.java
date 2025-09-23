58a4a34672197cfbc6a730f66772ed4ef4130f3e
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2025-09-23 14:38:53 +0000

    rafael

--- src/main/java/com/rafael/healthtracker/HealthTrackerApiApplication.java
+++ src/main/java/com/rafael/healthtracker/HealthTrackerApiApplication.java
@@ -3,6 +3,16 @@ package com.rafael.healthtracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "https://jubilant-space-xylophone-pjrwr664qpw5c9rq4-3335.app.github.dev/")
+    }
+)
+
 @SpringBootApplication
 public class HealthTrackerApiApplication {
 
